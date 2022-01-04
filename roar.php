6822b5d232ef2978e2778483fb13741e1b65126e
Look Alive <47085760+jpowelliv@users.noreply.github.com>
2022-01-04 09:47:49 -0600

    Add files via upload
    a program that uses a loop to print the integers 1 to 100, one number on each line. When a number is divisible by 5, print "Roar" on the same line, and separate them by a space. When a number is divisible by 7, print "Lions"; and when a number is divisible by 35, print "Roar Lions!".

---
+++ roar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Joseph Powell IV</h1>
+<h2>"CIS 476 HTTP SERVER"</h2>
+<?php
+echo "<br>";
+for ($a = 1; $x <= 100; $a++) {
+
+if ($a % 35 == 0) {
+echo "$a Roar<br>";
+
+}elseif ($a % 5 == 0) {
+echo "$a Lions <br>";
+
+}elseif ($a % 7 == 0) {
+echo "$a Roar Lions!<br>";
+
+}else 
+echo "$a<br>";
+}
+?>
+
+</body>
+</html>
